9cbcbf8b2b628b4fd40261d1483a2d69591e3fc3
Anshu Yaduvanshi <anshuyaduvanshi196@gmail.com>
2024-06-23 01:11:27 +0530

    set up new config for prod database

--- Backend/components/db.js
+++ Backend/components/db.js
@@ -1,23 +1,38 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const {
+  DEV_PG_USER,
+  DEV_PG_PASSWORD,
+  DEV_PG_HOST,
+  DEV_PG_PORT,
+  DEV_PG_DATABASE,
+  PROD_PG_USER,
+  PROD_PG_PASSWORD,
+  PROD_PG_HOST,
+  PROD_PG_PORT,
+  PROD_PG_DATABASE,
+  NODE_ENV,
+} = process.env;
+
 const devConfig = {
-  user: process.env.DEV_PG_USER,
-  password: process.env.DEV_PG_PASSWORD,
-  host: process.env.DEV_PG_HOST,
-  port: process.env.DEV_PG_PORT,
-  database: process.env.DEV_PG_DATABASE,
+  user: DEV_PG_USER,
+  password: DEV_PG_PASSWORD,
+  host: DEV_PG_HOST,
+  port: DEV_PG_PORT,
+  database: DEV_PG_DATABASE,
 };
 
 const prodConfig = {
-  connectionString: process.env.PROD_DATABASE_URL,
+  user: PROD_PG_USER,
+  password: PROD_PG_PASSWORD,
+  host: PROD_PG_HOST,
+  port: PROD_PG_PORT,
+  database: PROD_PG_DATABASE,
   ssl: {
     rejectUnauthorized: false,
   },
 };
-
-const pool = new Pool(
-  process.env.NODE_ENV === 'production' ? prodConfig : devConfig
-);
+const pool = new Pool(NODE_ENV === 'production' ? prodConfig : devConfig);
 
 module.exports = pool;
